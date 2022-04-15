8874da42b2764a25b6e5333e6dd37da15a7d8cbb
Enzo Piriou <enzopiriou@gmail.com>
2022-04-15 16:28:04 +0200

    fix synchro typo, add comments

--- src/handle_provide_token.c
+++ src/handle_provide_token.c
@@ -1,7 +1,7 @@
 #include "nested_plugin.h"
 
 // EDIT THIS: Adapt this function to your needs! Remember, the information for tokens are held in
-// `msg->token1` and `msg->token2`. If those pointers are `NULL`, this means the ethereum app didn't
+// `msg->item1` and `msg->item2`. If those pointers are `NULL`, this means the ethereum app didn't
 // find any info regarding the requested tokens!
 
 void handle_provide_token(void *parameters)
@@ -9,14 +9,12 @@ void handle_provide_token(void *parameters)
     ethPluginProvideInfo_t *msg = (ethPluginProvideInfo_t *)parameters;
     context_t *context = (context_t *)msg->pluginContext;
 
+    // The Ethereum App found the information for the requested token!
     if (msg->item1)
     {
-        // The Ethereum App found the information for the requested token!
         context->booleans |= TOKEN1_FOUND;
-
         // Store its decimals.
         context->token1_decimals = msg->item1->token.decimals;
-
         // Store its ticker.
         strlcpy(context->token1_ticker, (char *)msg->item1->token.ticker, sizeof(context->token1_ticker));
     }
@@ -24,18 +22,20 @@ void handle_provide_token(void *parameters)
     {
         PRINTF("handle_provide_token NO item1\n");
     }
+
+    // The Ethereum App found the information for the requested token!
     if (msg->item2)
     {
         context->booleans |= TOKEN2_FOUND;
-        // Only the ticker is stored.
+        // Only the ticker is stored for token2.
         strlcpy(context->token2_ticker, (char *)msg->item2->token.ticker, sizeof(context->token2_ticker));
     }
+
     if (context->selectorIndex == RELEASE_TOKENS)
     {
         if (context->number_of_tokens == 2 && context->booleans & TOKEN1_FOUND && context->booleans & TOKEN2_FOUND)
-        {
             msg->additionalScreens++;
-        }
     }
+
     msg->result = ETH_PLUGIN_RESULT_OK;
 }
