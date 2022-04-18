9994238dbb06ef7d93c51f773eda4bbc09e9ffa3
Enzo Piriou <enzopiriou@gmail.com>
2022-04-18 17:23:43 +0200

    clean finanlize

--- src/handle_finalize.c
+++ src/handle_finalize.c
@@ -1,17 +1,5 @@
 #include "nested_plugin.h"
 
-static void print_booleans(context_t *context)
-{
-    PRINTF("IS_COPY %d\n", context->booleans & IS_COPY);
-    PRINTF("TOKEN1_FOUND %d\n", context->booleans & TOKEN1_FOUND);
-    PRINTF("TOKEN2_FOUND %d\n", context->booleans & TOKEN2_FOUND);
-    PRINTF("IS_FROM_RESERVE %d\n", context->booleans & IS_FROM_RESERVE);
-    PRINTF("BOOL5 %d\n", context->booleans & BOOL5);
-    PRINTF("BOOL6 %d\n", context->booleans & BOOL6);
-    PRINTF("BOOL7 %d\n", context->booleans & BOOL7);
-    PRINTF("BOOL8 %d\n", context->booleans & BOOL8);
-}
-
 void handle_finalize(void *parameters)
 {
     ethPluginFinalize_t *msg = (ethPluginFinalize_t *)parameters;
@@ -19,6 +7,8 @@ void handle_finalize(void *parameters)
 
     // set default decimals
     context->token1_decimals = DEFAULT_DECIMAL;
+    // set default numScreens
+    msg->numScreens = 2;
 
     // Determine screens count.
     switch ((selector_t)context->selectorIndex)
@@ -46,39 +36,53 @@ void handle_finalize(void *parameters)
             msg->numScreens = 2;
             break;
         case NONE:
+        default:
             PRINTF("Error: could not find ui selector.\n");
-            msg->numScreens = 2;
-            break;
+            msg->result = ETH_PLUGIN_RESULT_ERROR;
+            return;
         }
         break;
+    default:
+        PRINTF("Param not supported: %d\n", context->next_param);
+        msg->result = ETH_PLUGIN_RESULT_ERROR;
+        return;
     }
 
     // Check if token1 is (0xeee...)
     if (ADDRESS_IS_NETWORK_TOKEN(context->token1_address))
         context->booleans |= TOKEN1_FOUND;
+    // Address is not network token (0xeee...) so we will need to look up the token.
     else
     {
-        PRINTF("Setting token1 address to: %.*H\n",
+        PRINTF("Setting tokenLookup1 to: %.*H\n",
                ADDRESS_LENGTH,
                context->token1_address);
-        // Address is not network token (0xeee...) so we will need to look up the token.
         msg->tokenLookup1 = context->token1_address;
     }
 
     // Check if token2 is (0xeee...) or (0x000...)
     if (ADDRESS_IS_NETWORK_TOKEN(context->token2_address) || ADDRESS_IS_NULL_ADDRESS(context->token2_address))
         context->booleans |= TOKEN2_FOUND;
+    // Address is not network token (0xeee...) or null so we will need to look up the token.
     else
     {
-        PRINTF("Setting token2 address to: %.*H\n",
+        PRINTF("Setting tokenLookup2 to: %.*H\n",
                ADDRESS_LENGTH,
                context->token2_address);
-        // Address is not network token (0xeee...) or null so we will need to look up the token.
         msg->tokenLookup2 = context->token2_address;
     }
 
-    print_booleans(context);
-    PRINTF("number_of_tokens: %d\n", context->number_of_tokens);
+    PRINTF("Booleans:\n");
+    PRINTF("IS_COPY %d\n", context->booleans & IS_COPY);
+    PRINTF("TOKEN1_FOUND %d\n", context->booleans & TOKEN1_FOUND);
+    PRINTF("TOKEN2_FOUND %d\n", context->booleans & TOKEN2_FOUND);
+    PRINTF("IS_FROM_RESERVE %d\n", context->booleans & IS_FROM_RESERVE);
+    PRINTF("BOOL5 %d\n", context->booleans & BOOL5);
+    PRINTF("BOOL6 %d\n", context->booleans & BOOL6);
+    PRINTF("BOOL7 %d\n", context->booleans & BOOL7);
+    PRINTF("BOOL8 %d\n", context->booleans & BOOL8);
+
+    PRINTF("\nnumber_of_tokens: %d\n", context->number_of_tokens);
     PRINTF("token1_amount: %.*H\n", sizeof(context->token1_amount), context->token1_amount);
 
     msg->uiType = ETH_UI_TYPE_GENERIC;
